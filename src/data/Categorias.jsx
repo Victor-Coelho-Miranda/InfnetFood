e71445f583ecba0b16cb112f2bf54f5ce8d4beff
Victor Coelho <vcoelho398@gmail.com>
2026-04-05 00:15:51 -0300

    Página Inicial e Listagem de Categorias

---
+++ src/data/Categorias.jsx
@@ -0,0 +1,12 @@
+export const Categories = [
+{ id: "1", nome: "🍔 Lanches" },
+  { id: "2", nome: "🥤 Bebidas" },
+  { id: "3", nome: "🍰 Sobremesas" },
+  { id: "4", nome: "🍕 Pizzas" },
+  { id: "5", nome: "🥗 Saudáveis" },
+  { id: "6", nome: "🍣 Japonesa" },
+  { id: "7", nome: "🍝 Massas" },
+  { id: "8", nome: "🍖 Churrasco" },
+  { id: "9", nome: "🌮 Mexicana" },
+  { id: "10", nome: "🥐 Café da manhã" },
+];
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,13 +1,49 @@
-import { View, Text, Button } from "react-native";
+import { View, Text, FlatList, StyleSheet, Pressable } from "react-native";
+import { Categories } from "../../data/Categorias";
 
-export default function Home({ navigation }) {
+export default function Home() {
+  
+  function renderItem({ item }) {
+    return (
+      <Pressable style={styles.card}>
+        <Text style={styles.texto}>{item.nome}</Text>
+      </Pressable>
+    );
+  }
+
   return (
-    <View>
-      <Text>Tela Home</Text>
-      <Button 
-        title="Ir para Login" 
-        onPress={() => navigation.navigate("Login")} 
+    <View style={styles.container}>
+      <Text style={styles.titulo}>Categorias</Text>
+
+      <FlatList
+        data={Categories}
+        keyExtractor={(item) => item.id}
+        renderItem={renderItem}
+        showsVerticalScrollIndicator={false}
       />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 20,
+    backgroundColor: "#f5f5f5",
+  },
+  titulo: {
+    fontSize: 26,
+    fontWeight: "bold",
+    marginBottom: 20,
+  },
+  card: {
+    backgroundColor: "#fff",
+    padding: 20,
+    borderRadius: 12,
+    marginBottom: 15,
+    elevation: 3,
+  },
+  texto: {
+    fontSize: 18,
+  },
+});
